c0214cb3313f4aed02e6beb98cd1f4b9e2c9e642
videseva <esvides@unicesar.edu.co>
2023-05-22 09:14:43 -0500

    Parte de eva finalizada

---
+++ src/app/Models/Pokemon.ts
@@ -0,0 +1,8 @@
+
+export interface Pokemon{
+    id: number;
+    name: string;
+    type: string;
+    image: string;
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Post } from './Models/Post';
+import { User } from './Models/Users';
+import { PostService } from './Services/post.service';
+import { UsersService } from './Services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProyectoPatronesSoft';
+  post : Post[]=[];
+  users :User[] = [];
+  idUser = 1;
+
+
+  constructor(private postServices :PostService, private userServices :UsersService) { }
+
+  ngOnInit() {
+   this.getPost();
+   this.getUser();
+   this.getPostUser();
+  }
+  getPost(){
+    this.postServices.getPost().subscribe(result => {
+      this.post  = result;
+      console.log(this.post)
+    });
+  }
+  getUser(){
+    this.userServices.getUsers().subscribe(result => {
+      this.users  = result;
+      console.log(this.users)
+    });
+  }
+  getPostUser(){
+    this.postServices.getPostUser(this.idUser).subscribe(result => {
+      this.post = result;
+    });;
+  }
 }
